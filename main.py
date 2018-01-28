467779d8cf1f67b1b57857b7d0ffbe953f1f4180
nick <ntiner1@tamu.edu>
2018-01-28 09:26:44 -0600

    Changed leaveTime string for alexa speech

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def calcLeaveTime(arrivalTime, travelTime):
         Assuming that arrivalTime is a STRING. '''
     
     arrivalTime = datetime.datetime.strptime(arrivalTime[:-6], "%Y-%m-%dT%H:%M")
-    return arrivalTime - datetime.timedelta(seconds=travelTime)
+    return datetime.datetime.strptime(arrivalTime - datetime.timedelta(seconds=travelTime), "%p, on %A %B %d.") #time, on day of week, month, day
 
 def SMS(text,num):#,unix_time):
     print("In SMS Function")
